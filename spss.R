1c22d56d375c0cbb9dfd41a962031b3a8de215c0
leeate <leeate@gmail.com>
2014-08-19 19:05:40 +0200

    Added explanation

--- spss.R
+++ spss.R
@@ -1,3 +1,14 @@
+# Subset data.frames imported from SPSS via 'read.spss' {foreign}
+# while keeping their variable labels, by temporarily attaching
+# the labels to the end of variable names.
+#     1. argument takes the SPSS data.frame
+#     2. is the desired rows (defaults to all)
+#     3. is the desired colums (defaults to all)
+#     4. The 'sep' argument is the character used to 
+#        stick together / split up names and labels. Defaults to an '@' char.
+#           example: spssdata[1:10, 1:3] equals to
+#                    subsetdf(spssdata, 1:10, 1:3)
+
 subsetdf <- function(x, row=1:nrow(x), col=1:length(x), sep="@") {
       # Keeping labels by temporarily sticking it to colname
       nameList <- names(x)
